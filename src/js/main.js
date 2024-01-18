be234773bbc083b074ba1b6afa9704841487b78e
Jakub <jakubrafalski96@gmail.com>
2024-01-18 06:56:33 +0000

    video

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 const video = document.querySelector('video');
 
 // Dodaj zdarzenie dotknięcia ekranu
-document.addEventListener('touchstart', () => {
+document.addEventListener('load', () => {
 	video.play();
 });
