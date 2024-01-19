a5fba2810c41d7fd39e8d5ed5bb17f1ad295f419
Jakub <jakubrafalski96@gmail.com>
2024-01-19 18:38:53 +0000

    next vh adj

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,4 @@
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+// let vh = window.innerHeight * 0.01;
+// document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+document.getElementById('.header').style.height = window.innerHeight + 'px';
