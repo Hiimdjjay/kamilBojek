f43ff6a35e39a450199341039a9545c481e9c655
Jakub <jakubrafalski96@gmail.com>
2024-01-19 18:42:48 +0000

    next adj vh with qS

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
 // let vh = window.innerHeight * 0.01;
 // document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-document.getElementById('.header').style.height = window.innerHeight + 'px';
+document.querySelector('.header').style.height = window.innerHeight + 'px';
