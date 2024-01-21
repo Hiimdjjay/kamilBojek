7943b967554933578ff929c2bb143d8ed4cb8848
Jakub <jakubrafalski96@gmail.com>
2024-01-21 10:05:33 +0000

    next commit heh

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,1 @@
-// let vh = window.innerHeight * 0.01;
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
-
 document.querySelector('.header').style.height = window.innerHeight + 'px';
